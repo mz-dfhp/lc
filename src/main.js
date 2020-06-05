ee79c32d574857107bc9b8a455152ad5edb76726
lc <486836332@qq.com>
2020-06-05 17:27:49 +0800

    loginfinish

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+
+Vue.config.productionTip = false
+
+
+// 网络请求
+import axios from 'axios'
+//配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
+    }
+  ]
+})
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from代表聪哪个路径来
+  // next是一个函数
+  if (to.path == '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+
+
+})
+export default router
